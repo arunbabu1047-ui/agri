911ac7eea3fba6fe19f1de0fc347b57c192583f3
arun <arun.b@nissisoftwaresystems.com>
2026-09-19 01:47:00 +0530

    Serve SPA routes directly

--- worker.js
+++ worker.js
@@ -1,10 +1,19 @@
 export default {
   async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request);
-    if (response.status === 404 && request.method === 'GET') {
-      const fallbackUrl = new URL('/index.html', request.url);
+    const url = new URL(request.url);
+    const isPageRequest = request.method === "GET" && !url.pathname.includes(".");
+
+    if (isPageRequest) {
+      const fallbackUrl = new URL("/index.html", request.url);
       return env.ASSETS.fetch(new Request(fallbackUrl, request));
     }
+
+    const response = await env.ASSETS.fetch(request);
+    if (response.status === 404 && request.method === "GET") {
+      const fallbackUrl = new URL("/index.html", request.url);
+      return env.ASSETS.fetch(new Request(fallbackUrl, request));
+    }
+
     return response;
   },
 };
